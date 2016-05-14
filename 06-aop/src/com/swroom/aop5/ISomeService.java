5fb5aeb0ec61c0df556da2301863622b843259ca
raykr <ray_kr@163.com>
2016-05-14 16:23:14 +0800

    NameMatchMethodPointcutAdvisor

---
+++ 06-aop/src/com/swroom/aop5/ISomeService.java
@@ -0,0 +1,10 @@
+package com.swroom.aop5;
+
+/**
+ * Created by Ray on 2016/5/9.
+ */
+public interface ISomeService {
+    void doSome();
+    String doSecond();
+    int doThird();
+}
---
+++ 06-aop/src/com/swroom/aop5/MyTest.java
@@ -0,0 +1,26 @@
+package com.swroom.aop5;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by Ray on 2016/4/29.
+ */
+public class MyTest {
+
+    @Test
+    public void test01() {
+
+        String configLocation = "com/swroom/aop5/applicationContext.xml";
+        ApplicationContext ac = new ClassPathXmlApplicationContext(configLocation);
+
+        ISomeService service = (ISomeService) ac.getBean("serviceProxy");
+        service.doSome();
+        System.out.println("--------------------------");
+        service.doSecond();
+        System.out.println("--------------------------");
+        service.doThird();
+
+    }
+}
