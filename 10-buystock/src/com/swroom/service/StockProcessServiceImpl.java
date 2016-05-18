48344204bc7e823f98ee48496dc9ee39b0d73b59
raykr <ray_kr@163.com>
2016-05-18 14:18:47 +0800

    tx-annotation

--- 10-buystock/src/com/swroom/service/StockProcessServiceImpl.java
+++ 10-buystock/src/com/swroom/service/StockProcessServiceImpl.java
@@ -3,9 +3,6 @@ package com.swroom.service;
 import com.swroom.beans.StockException;
 import com.swroom.dao.IAccountDao;
 import com.swroom.dao.IStockDao;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by Ray on 2016/5/17.
@@ -24,19 +21,16 @@ public class StockProcessServiceImpl implements IStockProcessService {
     }
 
     @Override
-    @Transactional(isolation = Isolation.DEFAULT, propagation = Propagation.REQUIRED)
     public void openAccount(String aname, double money) {
         accountDao.insertAccount(aname, money);
     }
 
     @Override
-    @Transactional(isolation = Isolation.DEFAULT, propagation = Propagation.REQUIRED)
     public void openStock(String sname, int amount) {
         stockDao.insertStock(sname, amount);
     }
 
     @Override
-    @Transactional(isolation = Isolation.DEFAULT, propagation = Propagation.REQUIRED, rollbackFor = StockException.class)
     public void buyStock(String aname, double money, String sname, int amount) throws StockException {
         boolean isBuy = true;
         accountDao.updateAccount(aname, money, isBuy);
